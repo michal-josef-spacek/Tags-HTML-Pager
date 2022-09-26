8272f0fba04f9b23fce74130b9fe488e748b0996
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2022-09-26 13:12:47 +0200

    Prepare for 0.04 version

--- Pager.pm
+++ Pager.pm
@@ -11,7 +11,7 @@ use Unicode::UTF8 qw(decode_utf8);
 
 Readonly::Scalar our $NUMBER_OF_BOXES => 7;
 
-our $VERSION = 0.03;
+our $VERSION = 0.04;
 
 # Constructor.
 sub new {
@@ -535,6 +535,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.03
+0.04
 
 =cut
